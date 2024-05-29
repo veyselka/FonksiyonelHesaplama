27bae76803ac6a8e1d53238dc328319687e981e2
Veysel Kılıçerkan <146098545+veyselka@users.noreply.github.com>
2024-05-29 21:13:21 +0300

    .

---
+++ LabÖdevHafta5/LabÖdevHafta5/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace LabÖdevHafta5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mutlak";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(146, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Karekök";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(249, 130);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Max";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(44, 195);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Sin";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(125, 195);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Cos";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(246, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "\"a,b\" şeklinde giriniz";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(280, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(249, 240);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Geri Al";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(44, 282);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(280, 156);
+            this.panel1.TabIndex = 8;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(33, 119);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(56, 20);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "COS";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(33, 79);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(50, 20);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "SİN";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(183, 79);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 4;
+            this.button7.Text = "Çiz";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(126, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "-";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(143, 33);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(71, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(52, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(68, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Çizim";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 466);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ LabÖdevHafta5/LabÖdevHafta5/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabÖdevHafta5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] sayilar;
+        int max;
+        double PI = 3.14159265359;
+        string sonuç = "";
+
+        Stack stack=new Stack();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public double Mutlak_Hesaplama(double Mutlak)
+        {
+            
+            if(Mutlak<0) {
+                Mutlak *= -1;
+            }
+            else
+            {
+                Mutlak = Mutlak;
+            }
+
+            return Mutlak;
+        }
+
+        public double Karekök_Hesaplama(double sayı)
+        {
+            double x = sayı;
+            double epsilon = 0.00001;
+            while (true)
+            {
+                x = 0.5 * (x + sayı / x);
+                if (Mutlak_Hesaplama(sayı - x * x) < epsilon)
+                {
+                    return x;
+                }
+            }
+        }
+
+        double Sin(double angle)
+        {
+            angle %= 2 * PI;
+            double sin = 0;
+            for (int i = 0; i < 84; i++)
+            {
+                sin += Us(-1, i) * (Us(angle, 2 * i + 1) / Fac(2 * i + 1));
+            }
+            return sin;
+        }
+
+        double Cos(double angle)
+        {
+            angle %= 2 * PI;
+            double cos = 0;
+            for (int i = 0; i < 84; i++)
+            {
+                cos += Us(-1, i) * (Us(angle, 2 * i) / Fac(2 * i));
+            }
+            return cos;
+        }
+
+        double Fac(int sayı)
+        {
+            if (sayı == 0)
+            {
+                return 1;
+            }
+            double result = 1;
+            for (int i = 1; i <= sayı; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        double Us(double sayı, int üs)
+        {
+            double sonuç = 1;
+            for (int i = 0; i < üs; i++)
+            {
+                sonuç *= sayı;
+            }
+            return sonuç;
+        }
+
+
+
+
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sonuc=Mutlak_Hesaplama(Convert.ToInt32(textBox1.Text));
+            textBox1.Text=Convert.ToString(sonuc);
+            stack.Push(sonuc);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double sonuc = Karekök_Hesaplama(Convert.ToDouble(textBox1.Text));
+            textBox1.Text = Convert.ToString(sonuc);
+            stack.Push(sonuc);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sayilar = textBox1.Text.Split(',');
+            max = int.MinValue;
+            foreach (string i in sayilar)
+            {
+                if (int.TryParse(i, out int num))
+                {
+                    if (num > max)
+                    {
+                        max = num;
+                    }
+                }
+            }
+            textBox1.Text = max.ToString();
+            stack.Push(max);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double sayi = Convert.ToDouble(textBox1.Text);
+            double sonuc = Math.Sin(sayi);
+            sonuç = textBox1.Text;
+            textBox1.Text = sonuc.ToString();
+            stack.Push(sonuc);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double sayi = Convert.ToDouble(textBox1.Text);
+            double sonuc = Cos(sayi);
+            sonuç = textBox1.Text;
+            textBox1.Text = sonuc.ToString();
+            stack.Push(sonuc);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (stack.Count == 0)
+            {
+                MessageBox.Show("Başka Eleman Yok !", "Uyarı", MessageBoxButtons.OK);
+            }
+            else
+            {
+                textBox1.Text = stack.Pop().ToString();
+
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Form form = new Form();
+            form.Text = "Çizim";
+            form.Size = new Size(600, 400);
+            form.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Dock = DockStyle.Fill;
+            pictureBox.BackColor = Color.White;
+            form.Controls.Add(pictureBox);
+
+            int startX = Convert.ToInt32(textBox2.Text);
+            int endX = Convert.ToInt32(textBox3.Text);
+
+            form.Show();
+
+            Bitmap bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            Pen Sinpen = new Pen(Color.Blue);
+            Pen Cospen = new Pen(Color.Red);
+
+            float scale = 50;
+            if (radioButton1.Checked)
+            {
+                for (int i = 0; i < pictureBox.Width; i++)
+                {
+                    double xValue = startX + (endX - startX) * (float)i / pictureBox.Width;
+                    float y = (float)(Sin(xValue * PI / 180) * scale + pictureBox.Height / 2);
+                    graphics.DrawRectangle(Sinpen, i, y, 1, 1);
+                }
+            }
+            if (radioButton2.Checked)
+            {
+                for (int i = 0; i < pictureBox.Width; i++)
+                {
+                    double xValue = startX + (endX - startX) * (float)i / pictureBox.Width;
+                    float y = (float)(Cos(xValue * PI / 180) * scale + pictureBox.Height / 2);
+                    graphics.DrawRectangle(Cospen, i, y, 1, 1);
+                }
+            }
+
+            pictureBox.Image = bitmap;
+        }
+
+        
+    }
+    
+}
